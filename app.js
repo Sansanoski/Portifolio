4491662105d890a9ff800f88cbc5187d2496df7b
wallace sansanoski <mendessansanoski@gmail.com>
2023-03-24 20:53:51 -0300

    refatoração de algumas funcionalidades

--- app.js
+++ app.js
@@ -1,106 +1,115 @@
-let spanText = document.querySelector('#spanID')
-const barMenu =document.querySelector('.barMenu')
-const containerLinks = document.querySelector('.containerLInk')
-const buttonGoTopPage = document.querySelector('.getTopPage')
-const ul = document.querySelector('ul')
-const closeMenuBar = document.querySelector('.closeMenuBar')
-const linksToNav = document.querySelectorAll('a')
-const stacksIcon = document.querySelectorAll('.stacks')
-const showCurriculum = document.querySelector('.resume')
-const divBlur = document.querySelector('.divBlur')
-const divCurriculo = document.querySelector('.divCurriculo')
-const divblur = document.querySelector('.divblur')
-const main = document.querySelector('main')
+let spanText = document.querySelector('[data-js="introduction-myName"]')
+const buttonGoTopPage = document.querySelector('[data-js="icon-to-up-and-down"]')
+const barMenu =document.querySelector('[ data-js="barMeu"]')
+const stacksIcon = document.querySelectorAll('[data-js="iconStack"]')
+const ULMenuBar = document.querySelector('[data-js="menu-bar"]')
+const containerLinks = document.querySelector('[data-js="container-lInk"]')
+const closeMenuBar = document.querySelector('[data-js="closeMenuBar"]')
+const getResume= document.querySelector('[data-js="resume"]')
+const divBlur = document.querySelector('[data-js="container-blur"]')
+const divCurriculo = document.querySelector('[data-js="container-resume"]')
+const mainContainer = document.querySelector('[data-js="main-container"]')
 
-function getText (text , counter ) {
-    if(counter < text.length) {
-        setTimeout(() => {
-            spanText.textContent += text.charAt(counter)
-            counter ++;
-            getText(text , counter)
-        } , 50)
-    }
+const  logIntroductionIntoScreen = (text, counter) => {
+
+  const isCounterMorethanTextLength = counter < text.length
+
+  if(isCounterMorethanTextLength) {
+    setTimeout(() => {
+      spanText.textContent += text.charAt(counter)
+      counter++
+      logIntroductionIntoScreen(text, counter)
+    }, 70)
   }
-  getText('WALLACE SANSANOSKI' , 0)
-  // const  insetNameScreen = (text , counter) => {
-  //   if(counter < text.length){
-  //    const idSetinterval2= setInterval(() => {
-  //       console.log(text.charAt(counter))
-  //       spanText.style.display = 'inline'
-  //       counter++
-  //     },1000)
-  //   }
-  // }
-  // insetNameScreen(spanText.textContent , 0)
+}
+  logIntroductionIntoScreen('WALLACE SANSANOSKI', 0)
 
 
-const closeBarMenu = event =>{
-    const clickedElement = event.target.tagName
-    if(clickedElement === 'A'|| clickedElement === 'SPAN'|| clickedElement === 'UL'||clickedElement=== 'LI'){
-      console.log(clickedElement)
-        ul.classList.remove('showUL')
-        barMenu.classList.remove('removeBarMenu')
-    }
+const closeBarMenu = event => {
+  const clickedElement = event.target.tagName
+  const clickedTagHTML = ['A', 'LI', 'SPAN', 'UL'].includes(clickedElement)
+
+  if (clickedTagHTML) {
+    ULMenuBar.classList.remove('showUL')
+    barMenu.classList.remove('removeBarMenu')
+  }
 }
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
- 
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  loop: true,
 
-  const showOrRemoveButtonGoToTopPage = ()=> {
-    if(window.pageYOffset >= 900){
-        buttonGoTopPage.classList.add('show')
-        return
-    }
-    else if(window.pageYOffset < 500){
-        buttonGoTopPage.classList.remove('show')
-        return
-    } 
-  }
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-const buttonGoUpPage =  event => {
-    window.scrollTo({
-        top : 0 ,
-        behavior :"smooth"
-    })
-  }
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-const openAbdCloseMenuBar = ()=> {
-  barMenu.classList.add('removeBarMenu')
-  ul.classList.add('showUL')
+});
+
+const showOrRemoveButtonGoToTopPage = () => {
+
+  const pageOFFSETisMoreThan900px = window.pageYOffset >= 900
+
+  pageOFFSETisMoreThan900px
+    ? buttonGoTopPage.classList.add('show')
+    : buttonGoTopPage.classList.remove('show')
+
 }
 
-buttonGoTopPage.addEventListener('click',buttonGoUpPage)
-window.addEventListener('scroll',showOrRemoveButtonGoToTopPage)
-ul.addEventListener('click' ,closeBarMenu)
-barMenu.addEventListener('click' , openAbdCloseMenuBar)
+const buttonGoUpPage = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+
+const openAndCloseMenuBar = () => {
+  barMenu.classList.add('removeBarMenu')
+  ULMenuBar.classList.add('showUL')
+}
+
+const stopAnimationIconStack = id => {
+  const pageWidthIsLessThan300 = document.documentElement.clientWidth < 300
+  if (pageWidthIsLessThan300) {
+    clearInterval(id)
+  }
+}
 
 const idSetinterval = setInterval(() => {
-  stacksIcon.forEach(item => {
-    item.classList.toggle('animate__bounce')
-  })
-  if(document.documentElement.clientWidth < 300){
-    clearInterval(2)
-  }
-},1000)
 
-showCurriculum.addEventListener('click', () => {
+  const insertAnimationClassIntoICON = item => {
+    item.classList.toggle('animate__bounce')
+  }
+
+  stacksIcon.forEach(insertAnimationClassIntoICON)
+
+  stopAnimationIconStack(idSetinterval)
+
+}, 1000)
+
+const showResumeIntoScreen = () => {
   divCurriculo.style.display ='flex'
-  divblur.style.display = 'block'
-})
-main.addEventListener('click' , event => {
-  if(event.target.tagName === 'DIV'|| event.target.tagName === 'SPAN'||event.target.tagName === 'IMG' ){
-    divCurriculo.style.display ='none'
-    divblur.style.display = 'none'
+  divBlur.style.display = 'block'
 }
-})
+
+const hideResumeIntoScreen = event => {
+  
+  const clickedElement = event.target.tagName
+  const isElementCliked = clickedElement === 'DIV'|| clickedElement === 'SPAN'||clickedElement === 'IMG'
+  
+  if(isElementCliked){
+    divCurriculo.style.display ='none'
+    divBlur.style.display = 'none'
+  }
+}
+
+getResume.addEventListener('click', showResumeIntoScreen)
+mainContainer.addEventListener('click', hideResumeIntoScreen)
+buttonGoTopPage.addEventListener('click', buttonGoUpPage)
+window.addEventListener('scroll', showOrRemoveButtonGoToTopPage)
+ULMenuBar.addEventListener('click', closeBarMenu)
+barMenu.addEventListener('click', openAndCloseMenuBar)
